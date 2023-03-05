ef972634930ae2b8815bfca497b09ef88b1dc8b2
JustinLien0805 <6998dragon@gmail.com>
2023-03-05 15:56:00 +0800

    change sphere to Octahedron

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,64 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.css";
+import * as THREE from "three";
+import gsap from "gsap";
+const scene = new THREE.Scene();
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const geometry = new THREE.OctahedronGeometry(5, 0);
+const material = new THREE.MeshPhysicalMaterial({
+  roughness: 1,
+  transmission: 1,
+});
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+// wireframe
+var geo = new THREE.WireframeGeometry(sphere.geometry); // or WireframeGeometry
+var mat = new THREE.LineBasicMaterial({ color: 0xffffff, linewidth: 2 });
+var wireframe = new THREE.LineSegments(geo, mat);
+sphere.add(wireframe);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const sizes = { width: window.innerWidth, height: window.innerHeight };
+
+// camera (FOV, aspect ratio, near(limit), far(limit))
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 10;
+scene.add(camera);
+
+// light
+const light = new THREE.PointLight(0xffffff, 2, 100);
+light.position.set(10, 10, 10);
+scene.add(light);
+
+const canvas = document.querySelector(".webgl") as HTMLCanvasElement;
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2);
+renderer.render(scene, camera);
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const animation = () => {
+  sphere.rotation.y += 0.001;
+  sphere.rotation.x += 0.0007;
+  requestAnimationFrame(animation);
+  renderer.render(scene, camera);
+};
+
+animation();
+
+const t1 = gsap.timeline({ defaults: { duration: 1 } });
+t1.fromTo(sphere.scale, { x: 0.1, y: 0.1, z: 0.1 }, { x: 1, y: 1, z: 1 });
+t1.fromTo(".navbar", { opacity: 0 }, { opacity: 1 }, "-=1");
